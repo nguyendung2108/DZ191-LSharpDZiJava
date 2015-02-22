7cd9d67cd673dcacbc1987795293916c4e123c89
iJava <iJava.i@hotmail.co.uk>
2015-02-22 22:11:11 +0000

    Resharper
    Resharper

--- iDzLucian/iDzLucian/iDZLucian.cs
+++ iDzLucian/iDzLucian/iDZLucian.cs
@@ -1,17 +1,17 @@
-﻿// This file is part of iDZLucian
+﻿// This file is part of LeagueSharp.Common.
 // 
-// iDZLucian is free software: you can redistribute it and/or modify
+// LeagueSharp.Common is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 // 
-// iDZLucian is distributed in the hope that it will be useful,
+// LeagueSharp.Common is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 // 
 // You should have received a copy of the GNU General Public License
-// along with iDZLucian.  If not, see <http://www.gnu.org/licenses/>.
+// along with LeagueSharp.Common.  If not, see <http://www.gnu.org/licenses/>.
 
 using System;
 using System.Collections.Generic;
@@ -30,14 +30,14 @@ namespace iDzLucian
         private static Spell _qExtended;
         public static Menu Menu;
         private static Orbwalking.Orbwalker _orbwalker;
-        private static bool shouldHavePassive;
+        private static bool _shouldHavePassive;
 
-        private static readonly Dictionary<SpellSlot, Spell> _spells = new Dictionary<SpellSlot, Spell>
+        private static readonly Dictionary<SpellSlot, Spell> Spells = new Dictionary<SpellSlot, Spell>
         {
-            {SpellSlot.Q, new Spell(SpellSlot.Q,675f)},
-            {SpellSlot.W, new Spell(SpellSlot.W,1000f)},
-            {SpellSlot.E, new Spell(SpellSlot.E,425f)},
-            {SpellSlot.R, new Spell(SpellSlot.R,1400f)},
+            { SpellSlot.Q, new Spell(SpellSlot.Q, 675f) },
+            { SpellSlot.W, new Spell(SpellSlot.W, 1000f) },
+            { SpellSlot.E, new Spell(SpellSlot.E, 425f) },
+            { SpellSlot.R, new Spell(SpellSlot.R, 1400f) }
         };
 
         public static void OnLoad(EventArgs args)
@@ -51,18 +51,19 @@ namespace iDzLucian
 
             LoadSpells();
             CreateMenu();
-            Notifications.AddNotification(new Notification("iDZLucian v"+Assembly.GetExecutingAssembly().GetName().Version+" loaded!", 2500));
+            Notifications.AddNotification(
+                new Notification("iDZLucian v" + Assembly.GetExecutingAssembly().GetName().Version + " loaded!", 2500));
             Game.OnGameUpdate += OnGameUpdate;
             Obj_AI_Base.OnProcessSpellCast += Obj_AI_Base_OnProcessSpellCast;
-            Orbwalking.AfterAttack += Orbwalking_AfterAttack;
+            Orbwalking.AfterAttack += OrbwalkingAfterAttack;
         }
 
-        static void Orbwalking_AfterAttack(AttackableUnit unit, AttackableUnit target)
+        private static void OrbwalkingAfterAttack(AttackableUnit unit, AttackableUnit target)
         {
-            shouldHavePassive = false;
+            _shouldHavePassive = false;
         }
 
-        static void Obj_AI_Base_OnProcessSpellCast(Obj_AI_Base sender, GameObjectProcessSpellCastEventArgs args)
+        private static void Obj_AI_Base_OnProcessSpellCast(Obj_AI_Base sender, GameObjectProcessSpellCastEventArgs args)
         {
             //TODO Get Correct spell names
             //Reset the AutoAttack timer after a Q, so we can attack immediately after.
@@ -73,10 +74,12 @@ namespace iDzLucian
                 switch (args.SData.Name)
                 {
                     case "LucianQ":
-                        Utility.DelayAction.Add((int)(Math.Ceiling(Game.Ping/2f)+250+325),Orbwalking.ResetAutoAttackTimer);
+                        Utility.DelayAction.Add(
+                            (int) (Math.Ceiling(Game.Ping / 2f) + 250 + 325), Orbwalking.ResetAutoAttackTimer);
                         break;
                     case "LucianW":
-                        Utility.DelayAction.Add((int)(Math.Ceiling(Game.Ping / 2f) + 250 + 375), Orbwalking.ResetAutoAttackTimer);
+                        Utility.DelayAction.Add(
+                            (int) (Math.Ceiling(Game.Ping / 2f) + 250 + 375), Orbwalking.ResetAutoAttackTimer);
                         break;
                     case "LucianE":
                         break;
@@ -106,31 +109,32 @@ namespace iDzLucian
                     break;
             }
         }
+
         private static void Combo()
         {
-            var target = TargetSelector.GetTarget(_spells[SpellSlot.Q].Range, TargetSelector.DamageType.Physical);
+            var target = TargetSelector.GetTarget(Spells[SpellSlot.Q].Range, TargetSelector.DamageType.Physical);
 
-            if (target.IsValidTarget(_spells[SpellSlot.Q].Range))
+            if (target.IsValidTarget(Spells[SpellSlot.Q].Range))
             {
-                if (_spells[SpellSlot.Q].IsEnabledAndReady(Mode.Combo))
+                if (Spells[SpellSlot.Q].IsEnabledAndReady(Mode.Combo))
                 {
-                    if (_spells[SpellSlot.Q].CanCast(target) && !(hasPassive() && Orbwalking.InAutoAttackRange(target)))
+                    if (Spells[SpellSlot.Q].CanCast(target) && !(HasPassive() && Orbwalking.InAutoAttackRange(target)))
                     {
-                        _spells[SpellSlot.Q].Cast(target);
+                        Spells[SpellSlot.Q].Cast(target);
                         _orbwalker.ForceTarget(target);
-                        shouldHavePassive = true;
+                        _shouldHavePassive = true;
                     }
                     else
                     {
                         ExtendedQ();
                     }
-                    
                 }
-                if (_spells[SpellSlot.W].IsEnabledAndReady(Mode.Combo) && !_spells[SpellSlot.Q].CanCast(target) && !(hasPassive() && Orbwalking.InAutoAttackRange(target)))
+                if (Spells[SpellSlot.W].IsEnabledAndReady(Mode.Combo) && !Spells[SpellSlot.Q].CanCast(target) &&
+                    !(HasPassive() && Orbwalking.InAutoAttackRange(target)))
                 {
-                    _spells[SpellSlot.W].Cast(target);
+                    Spells[SpellSlot.W].Cast(target);
                     _orbwalker.ForceTarget(target);
-                    shouldHavePassive = true;
+                    _shouldHavePassive = true;
                 }
             }
         }
@@ -138,16 +142,17 @@ namespace iDzLucian
         private static void ExtendedQ()
         {
             //Untested lmao
-            var target = TargetSelector.GetTarget(_spells[SpellSlot.Q].Range, TargetSelector.DamageType.Physical);
+            var target = TargetSelector.GetTarget(Spells[SpellSlot.Q].Range, TargetSelector.DamageType.Physical);
             var targetExtended = TargetSelector.GetTarget(_qExtended.Range, TargetSelector.DamageType.Physical);
             if (!target.IsValidTarget() && targetExtended.IsValidTarget())
             {
                 var targetPrediction = _qExtended.GetPrediction(targetExtended).UnitPosition.To2D();
-                var qCollision = _qExtended.GetCollision(ObjectManager.Player.ServerPosition.To2D(), new List<Vector2>() { targetPrediction });
+                var qCollision = _qExtended.GetCollision(
+                    ObjectManager.Player.ServerPosition.To2D(), new List<Vector2> { targetPrediction });
                 if (qCollision.Any())
                 {
-                    _spells[SpellSlot.Q].Cast(qCollision.First());
-                    shouldHavePassive = true;
+                    Spells[SpellSlot.Q].Cast(qCollision.First());
+                    _shouldHavePassive = true;
                 }
             }
         }
@@ -162,20 +167,21 @@ namespace iDzLucian
             throw new NotImplementedException();
         }
 
-        private static bool hasPassive()
+        private static bool HasPassive()
         {
-            return shouldHavePassive || ObjectManager.Player.HasBuff("lucianpassivebuff");
+            return _shouldHavePassive || ObjectManager.Player.HasBuff("lucianpassivebuff");
         }
-       
+
         #region Menu and Spells
+
         private static void LoadSpells()
         {
-            _spells[SpellSlot.Q].SetTargetted(0.25f, float.MaxValue);
+            Spells[SpellSlot.Q].SetTargetted(0.25f, float.MaxValue);
             _qExtended = new Spell(SpellSlot.Q, 1100);
             _qExtended.SetSkillshot(0.25f, 5f, float.MaxValue, true, SkillshotType.SkillshotLine);
-            _spells[SpellSlot.W].SetSkillshot(0.3f, 80, 1600, true, SkillshotType.SkillshotLine);
-            _spells[SpellSlot.E].SetSkillshot(.25f, 1f, float.MaxValue, false, SkillshotType.SkillshotLine);
-            _spells[SpellSlot.R].SetSkillshot(.1f, 110, 2800, true, SkillshotType.SkillshotLine);
+            Spells[SpellSlot.W].SetSkillshot(0.3f, 80, 1600, true, SkillshotType.SkillshotLine);
+            Spells[SpellSlot.E].SetSkillshot(.25f, 1f, float.MaxValue, false, SkillshotType.SkillshotLine);
+            Spells[SpellSlot.R].SetSkillshot(.1f, 110, 2800, true, SkillshotType.SkillshotLine);
         }
 
         private static void CreateMenu()
@@ -185,34 +191,34 @@ namespace iDzLucian
             var orbMenu = new Menu("Lucian - Orbwalker", "com.idzlucian.orbwalker");
             _orbwalker = new Orbwalking.Orbwalker(orbMenu);
             Menu.AddSubMenu(orbMenu);
-            
+
             var tsMenu = new Menu("Lucian - Target Selector", "com.idzlucian.ts");
             TargetSelector.AddToMenu(tsMenu);
             Menu.AddSubMenu(tsMenu);
 
             var comboMenu = new Menu("Lucian - Combo", "com.idzlucian.combo");
-            comboMenu.AddModeMenu(Mode.Combo,new []{SpellSlot.Q,SpellSlot.W,SpellSlot.E,SpellSlot.R},new []{true,true,true,false});
-            comboMenu.AddManaManager(Mode.Combo,new []{SpellSlot.Q,SpellSlot.W,SpellSlot.E,SpellSlot.R},new []{35,35,25,10});
+            comboMenu.AddModeMenu(
+                Mode.Combo, new[] { SpellSlot.Q, SpellSlot.W, SpellSlot.E, SpellSlot.R },
+                new[] { true, true, true, false });
+            comboMenu.AddManaManager(
+                Mode.Combo, new[] { SpellSlot.Q, SpellSlot.W, SpellSlot.E, SpellSlot.R }, new[] { 35, 35, 25, 10 });
             Menu.AddSubMenu(comboMenu);
 
             var harassMenu = new Menu("Lucian - Harass", "com.idzlucian.harass");
-            harassMenu.AddModeMenu(Mode.Harrass, new[] { SpellSlot.Q, SpellSlot.W}, new[] { true, true });
-            harassMenu.AddManaManager(Mode.Harrass, new[] { SpellSlot.Q, SpellSlot.W}, new[] { 35, 35 });
+            harassMenu.AddModeMenu(Mode.Harrass, new[] { SpellSlot.Q, SpellSlot.W }, new[] { true, true });
+            harassMenu.AddManaManager(Mode.Harrass, new[] { SpellSlot.Q, SpellSlot.W }, new[] { 35, 35 });
             Menu.AddSubMenu(harassMenu);
 
             var farmMenu = new Menu("Lucian - Farm", "com.idzlucian.farm");
-            farmMenu.AddModeMenu(Mode.Laneclear, new[] { SpellSlot.Q }, new[] { true});
+            farmMenu.AddModeMenu(Mode.Laneclear, new[] { SpellSlot.Q }, new[] { true });
             farmMenu.AddManaManager(Mode.Laneclear, new[] { SpellSlot.Q }, new[] { 35 });
             Menu.AddSubMenu(farmMenu);
 
-            var miscMenu = new Menu("Lucian - Misc", "com.idzlucian.misc")
-            {
-                //TODO Add Stuff
-            };
+            var miscMenu = new Menu("Lucian - Misc", "com.idzlucian.misc");
 
             Menu.AddToMainMenu();
         }
-        #endregion
 
+        #endregion
     }
 }
