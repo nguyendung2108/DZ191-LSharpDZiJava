fd2a516f2c68ced2c4c268ba9ae1922befab5034
iJava <iJava.i@hotmail.co.uk>
2015-02-23 17:30:10 +0000

    d
    d

--- iDzLucian/iDzLucian/Helpers/PositionHelper.cs
+++ iDzLucian/iDzLucian/Helpers/PositionHelper.cs
@@ -1,4 +1,19 @@
-﻿using System.Collections.Generic;
+﻿// This file is part of LeagueSharp.Common.
+// 
+// LeagueSharp.Common is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// LeagueSharp.Common is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with LeagueSharp.Common.  If not, see <http://www.gnu.org/licenses/>.
+
+using System.Collections.Generic;
 using System.Linq;
 using LeagueSharp;
 using LeagueSharp.Common;
@@ -6,12 +21,14 @@ using SharpDX;
 
 namespace iDzLucian.Helpers
 {
-    class PositionHelper
+    internal class PositionHelper
     {
         public static bool IsSafePosition(Vector3 position)
         {
             if (position.UnderTurret(true) && !ObjectManager.Player.UnderTurret(true))
+            {
                 return false;
+            }
             var allies = position.CountAlliesInRange(ObjectManager.Player.AttackRange);
             var enemies = position.CountEnemiesInRange(ObjectManager.Player.AttackRange);
             var lhEnemies = GetLhEnemiesNearPosition(position, ObjectManager.Player.AttackRange).Count();
@@ -24,9 +41,12 @@ namespace iDzLucian.Helpers
             //Adding 1 for the Player
             return (allies + 1 > enemies - lhEnemies);
         }
+
         public static List<Obj_AI_Hero> GetLhEnemiesNearPosition(Vector3 position, float range)
         {
-            return HeroManager.Enemies.Where(hero => hero.IsValidTarget(range, true, position) && hero.HealthPercentage() <= 15).ToList();
+            return
+                HeroManager.Enemies.Where(
+                    hero => hero.IsValidTarget(range, true, position) && hero.HealthPercentage() <= 15).ToList();
         }
     }
 }
