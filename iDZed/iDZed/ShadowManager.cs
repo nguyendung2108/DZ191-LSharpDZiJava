76a0513f23bdbaf6455a967772ec4e7dd17d908f
DZ191 <darkprince97@live.it>
2015-04-02 10:47:57 +0200

    iDZed

---
+++ iDZed/iDZed/ShadowManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using LeagueSharp;
+
+namespace iDZed
+{
+    class ShadowManager
+    {
+        private static List<Shadow> _shadowsList = new List<Shadow>
+        {
+            new Shadow { State = ShadowState.NotActive, Type = ShadowType.Normal },
+            new Shadow { State = ShadowState.NotActive, Type = ShadowType.Ult}
+        };
+        private const String ZedWShadowName = "";
+        private const String ZedRShadowName = "";
+        private const String ZedW2SsName = "";
+        private const String ZedR2SsName = "";
+
+        public static void OnLoad()
+        {
+            GameObject.OnCreate += GameObject_OnCreate;
+            GameObject.OnDelete += GameObject_OnDelete;
+        }
+        static void GameObject_OnCreate(GameObject sender, EventArgs args)
+        {
+            throw new NotImplementedException();
+        }
+
+        static void GameObject_OnDelete(GameObject sender, EventArgs args)
+        {
+            throw new NotImplementedException();
+        }  
+    }
+
+    class Shadow
+    {
+        public ShadowState State { get; set; }
+        public ShadowType Type { get; set; }
+    }
+
+    enum ShadowType
+    {
+        Normal,
+        Ult
+    }
+
+    enum ShadowState
+    {
+        NotActive,
+        Travelling,
+        Created,
+        Used
+    }
+}
---
+++ iDZed/iDZed/Zed.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+using LeagueSharp.Common;
+
+namespace iDZed
+{
+    class Zed
+    {
+        public static Menu _menu;
+        private static Orbwalking.Orbwalker _orbwalker;
+        private static Obj_AI_Hero Player { get { return ObjectManager.Player; } }
+        private delegate void OnOrbwalkingMode();
+        private static readonly Dictionary<SpellSlot, Spell> _spells = new Dictionary<SpellSlot, Spell>()
+        {
+            {SpellSlot.Q, new Spell(SpellSlot.Q, 900f)},
+            {SpellSlot.W, new Spell(SpellSlot.W, 550f)},
+            {SpellSlot.E, new Spell(SpellSlot.E, 290f)},
+            {SpellSlot.R, new Spell(SpellSlot.R, 625f)}
+        };
+        private static Dictionary<Orbwalking.OrbwalkingMode, OnOrbwalkingMode> _orbwalkingModesDictionary;
+
+        public static void OnLoad()
+        {
+            ShadowManager.OnLoad();
+            _orbwalkingModesDictionary = new Dictionary<Orbwalking.OrbwalkingMode, OnOrbwalkingMode>()
+            {
+                {Orbwalking.OrbwalkingMode.Combo, Combo},
+                {Orbwalking.OrbwalkingMode.Mixed, Harass},
+                {Orbwalking.OrbwalkingMode.LastHit, Farm},
+                {Orbwalking.OrbwalkingMode.LaneClear, Farm},
+                {Orbwalking.OrbwalkingMode.None, () => {}},
+            };
+            InitMenu();
+            InitSpells();
+            InitEvents();
+        }
+
+        #region Modes Region
+        private static void Combo()
+        {
+            throw new NotImplementedException();
+        }
+        private static void Harass()
+        {
+            throw new NotImplementedException();
+        }
+        private static void Farm()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region Initialization Region
+        private static void InitMenu()
+        {
+            _menu = new Menu("iDZed - Reloaded","com.idz.zed",true);
+            TargetSelector.AddToMenu(_menu);
+            var orbwalkMenu = new Menu("[iDZed] Orbwalker", "com.idz.zed.orbwalker");
+            _orbwalker = new Orbwalking.Orbwalker(orbwalkMenu);
+            var comboMenu = new Menu("[iDZed] Combo", "com.idz.zed.combo");
+            {
+                comboMenu.AddItem(new MenuItem("com.idz.zed.combo.useq", "Use Q"));
+                comboMenu.AddItem(new MenuItem("com.idz.zed.combo.usew", "Use W"));
+                comboMenu.AddItem(new MenuItem("com.idz.zed.combo.usee", "Use E"));
+                comboMenu.AddItem(new MenuItem("com.idz.zed.combo.user", "Use R"));
+                comboMenu.AddItem(new MenuItem("com.idz.zed.combo.swapw", "Swap W For Follow"));
+                comboMenu.AddItem(new MenuItem("com.idz.zed.combo.swapr", "Swap R On kill"));
+            };
+            _menu.AddSubMenu(comboMenu);
+            _menu.AddToMainMenu();
+        }
+
+        private static void InitSpells()
+        {
+            _spells[SpellSlot.Q].SetSkillshot(0.25f, 50f, 1700f, false, SkillshotType.SkillshotLine);
+        }
+
+        private static void InitEvents()
+        {
+            Game.OnUpdate += Game_OnUpdate;
+        }
+        #endregion
+
+        #region Events Region
+        static void Game_OnUpdate(EventArgs args)
+        {
+            _orbwalkingModesDictionary[_orbwalker.ActiveMode]();
+        }
+        #endregion
+    }
+}
